fe2c3895fa98815908e0a55e99cd40d9d47ec60f
Todd Murphy <tmurphy605@icloud.com>
2019-09-25 19:49:57 -0400

    added register.php

---
+++ register.php
@@ -0,0 +1,97 @@
+<html>
+	<head>
+		<title>IS218 Project 1 Login</title>
+	</head>
+	<body>
+	<style>
+		//body{
+		//	background-color: #cfb53b;
+		//}
+	</style>
+		<div align=center>
+			<h2>IS218 Project 1 Login</h2>
+        
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        
+        	<div align = center style="background-color:#663399;width:19%;border-radius:15px;margain:0 auto;">
+                	<br>
+	    		<div style="max-width:90%;">
+	    		<form action = "login.php" method=post>
+                		<fieldset id="field"><legend>Login</legend>
+                    
+                        <?php
+                            //error reporting code
+                            error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+                            ini_set('display_errors' , 1);
+
+                            $firstName = $_POST ['firstName'];
+                            $lastName = $_POST ['lastName'];
+                            $birthday = $_POST ['birthday'];
+                            $email = $_POST ['email'];
+                            $pass = $_POST ['pass'];
+                            
+                            //Check First Name for requirements
+                            if (empty($firstName)){
+                                $out = "First Name cannot be empty!";
+                                $valid = false;
+                            }
+                            
+                            //Check Last Name for requirements
+                            if (empty($lastName)){
+                                $out = "Last Name cannot be empty!";
+                                $valid = false;
+                            }
+                            
+                            //Check Birthday for requirements
+                            if (empty($birthday)){
+                                $out = "Birthday cannot be empty!";
+                                $valid = false;
+                            }
+
+                            //Check Email for requirements
+                            $contains_symbol = strpos($email, '@') !== false;
+
+                            if (empty($email)){
+                                $out = "Email cannot be empty!";
+                                $valid = false;
+                            }
+
+                            if (!$contains_symbol){
+                                $out = "Email does not contain @ symbol!";
+                                $valid = false;
+                            }
+
+
+                            //Check Password for requirements
+                            if (empty($pass)){
+                                $out = "Password cannot be empty!";
+                                $valid = false;
+                            }
+                            if (strlen($pass) <= 8){
+                                $out = "Password must be at least 8 characters!";
+                                $valid = false;
+                            }
+
+                            //if they made it past the checks
+                            if ($valid){
+                                $out = "Congrats. You made it!";
+                            }
+                            
+                            //print out
+                            print "<span>$out</span>";
+                        ?>
+
+
+                		</fieldset>
+
+            		</form>
+	    		<br>
+	     		</div>
+        	</div>
+        	</div>
+	</body>
+</html>
