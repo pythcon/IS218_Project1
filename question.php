c022fc0a1b5d9a7f567a0d230f7d0ed7051dc67f
Todd Murphy <tmurphy605@icloud.com>
2019-09-25 23:46:42 -0400

    added question.php

---
+++ question.php
@@ -0,0 +1,80 @@
+<html>
+	<head>
+		<title>IS218 Project 1 Login</title>
+	</head>
+	<body>
+	<style>
+		//body{
+		//	background-color: #cfb53b;
+		//}
+	</style>
+		<div align=center>
+			<h2>IS218 Project 1 New Question Form</h2>
+        
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        
+        	<div align = center style="background-color:#663399;width:19%;border-radius:15px;margain:0 auto;">
+                	<br>
+	    		<div style="max-width:90%;">
+	    		<form action = "login.php" method=post>
+                		<fieldset id="field"><legend>New Question Form</legend>
+                    
+                        <?php
+                            //error reporting code
+                            error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+                            ini_set('display_errors' , 1);
+
+                            $questionName = $_POST ['questionName'];
+                            $questionBody = $_POST ['questionBody'];
+                            $questionSkills = $_POST ['questionSkills'];
+                            $out = "";
+                            
+                            //Parse array
+                            $containsComma = strpos($questionSkills, ',') !== false;
+                            
+                            if ()
+                            
+                            //Check Question Name for requirements
+                            if (empty($questionName)){
+                                $out .= "Question Name cannot be empty!<br>";
+                            }
+                            
+                            if (strlen($questionName) <= 3){
+                                $out .= "Question Name must be at least 3 characters!<br>";
+                            }
+                            
+                            //Check Question Body for requirements
+                            if (empty($questionBody)){
+                                $out .= "Question Body cannot be empty!<br>";
+                            }
+                            
+                            if (strlen($questionBody) <= 500){
+                                $out .= "Question Body must be at least 500 characters!<br>";
+                            }
+                            
+                            //Check Question Skills for requirements
+                            if ($questionSkills.length <= 2){
+                                $out .= "There must be at least 2 skills!<br>";
+                            }
+
+                            //if they made it past the checks
+                                
+                            
+                            //print out
+                            print "<span>$out</span>";
+                        ?>
+
+
+                		</fieldset>
+
+            		</form>
+	    		<br>
+	     		</div>
+        	</div>
+        	</div>
+	</body>
+</html>
