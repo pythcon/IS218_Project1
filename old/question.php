8f551ba64cc67845425ac0a4ad140cd22c93b347
Todd Murphy <tam1998@comcast.net>
2019-11-14 01:58:39 -0500

    redone all of css styling

--- old/question.php
+++ old/question.php
@@ -1,14 +1,29 @@
 <html>
 	<head>
-		<title>IS218 Project 1 Question</title>
-        <link rel="stylesheet" href="css/style.css">
+		<title>IS218 Project 1 New Question Form</title>
 	</head>
-    <body>
-        <div class="mainContainer">
-            <h2>IS218 Project 1 Question</h2>
-        	<div>
-                <div class="formContainer">
-                    <?php
+	<body>
+	<style>
+		//body{
+		//	background-color: #cfb53b;
+		//}
+	</style>
+		<div align=center>
+			<h2>IS218 Project 1 New Question Form</h2>
+        
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        
+        	<div align = center style="background-color:#663399;width:19%;border-radius:15px;margain:0 auto;">
+                	<br>
+	    		<div style="max-width:90%;">
+	    		<form action = "login.php" method=post>
+                		<fieldset id="field"><legend>New Question Form</legend>
+                    
+                        <?php
                             //error reporting code
                             error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
                             ini_set('display_errors' , 1);
@@ -69,8 +84,14 @@
                             //print out
                             print "<span>$out</span>";
                         ?>
-                </div>
+
+
+                		</fieldset>
+
+            		</form>
+	    		<br>
+	     		</div>
         	</div>
-        </div>
+        	</div>
 	</body>
 </html>
--- old/register.php
+++ old/register.php
@@ -1,14 +1,29 @@
 <html>
 	<head>
 		<title>IS218 Project 1 Register</title>
-        <link rel="stylesheet" href="css/style.css">
 	</head>
-    <body>
-        <div class="mainContainer">
-            <h2>IS218 Project 1 Register</h2>
-        	<div>
-                <div class="formContainer">
-                    <?php
+	<body>
+	<style>
+		//body{
+		//	background-color: #cfb53b;
+		//}
+	</style>
+		<div align=center>
+			<h2>IS218 Project 1 Register</h2>
+        
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        
+        	<div align = center style="background-color:#663399;width:19%;border-radius:15px;margain:0 auto;">
+                	<br>
+	    		<div style="max-width:90%;">
+	    		<form action = "login.php" method=post>
+                		<fieldset id="field"><legend>Register</legend>
+                    
+                        <?php
                             //error reporting code
                             error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
                             ini_set('display_errors' , 1);
@@ -95,8 +110,14 @@
                             //print out
                             print "<span>$out</span>";
                         ?>
-                </div>
+
+
+                		</fieldset>
+
+            		</form>
+	    		<br>
+	     		</div>
         	</div>
-        </div>
+        	</div>
 	</body>
 </html>
