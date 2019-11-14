d4cc6fbadc9a2db353d316f4cc0b165208a8b16f
Todd Murphy <tam1998@comcast.net>
2019-11-14 15:30:34 -0500

    fixed printout

--- accountPage.php
+++ accountPage.php
@@ -39,7 +39,7 @@
                                 $q->execute();
                                 $results = $q->fetchAll();
 
-                                $out .= "<table cellpadding='2px'>";
+                                $out .= "<table border='2px'>";
                                 $out .= "<tr><td>Name</td><td>Body</td><td>Skills</td></tr>";
                                 foreach ($results as $row){
                                     $out .= "<tr><td>".$row['title']."</td><td>".$row['body']."</td><td>".$row['skills']."</td></tr>";
